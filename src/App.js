a729b9c780d69541ab977fbe71176ec6d4aa77f4
asanIbrahim <asanib98@gmail.com>
2025-06-15 03:16:39 +0530

    create nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import './App.css';
-import  Home from'./Pages/Home/Home';
-import Products from './Pages/Products/Products'
-import ProductDetails from './Pages/ProductDetails/ProductDetails'
-import Cart from './Pages/Cart/Cart'
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import Cart from "./Pages/Cart/Cart";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-       App
-     <Home/>
-     <Products/>
-     <ProductDetails/>
-     <Cart/>
-    </div>
+    <>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/productDetails/:items" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<div>Page Not found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
