b63a110acf4b820bece318bbca884d1949ec8fb4
asanIbrahim <asanib98@gmail.com>
2025-06-25 23:58:15 +0530

    add package

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import Home from "./Pages/Home/Home";
 import Products from "./Pages/Products/Products";
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
 import Cart from "./Pages/Cart/Cart";
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/NavBar/Navbar";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/productDetails/:items" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<div>Page Not found</div>} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Provider store={store}>
+      <>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/productDetails/:items" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<div>Page Not found</div>} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    </Provider>
   );
 }
 
